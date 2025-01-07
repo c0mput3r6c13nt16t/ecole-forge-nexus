d30e9c8fcf7aaf4d8e3250a1ed21f5bd52a892b0
c0mput3r6c13nt16t <d3v310p3r.gn0mad@gmail.com>
2025-01-08 01:47:04 +0900

    submit

--- learning/courses/current/fourtwo/piscine/shell-01/ex02/find_sh.sh
+++ learning/courses/current/fourtwo/piscine/shell-01/ex02/find_sh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -name "*.sh" | xargs -i basename {} | sed -e 's/.sh$//' | sort
+find . -type f -name "*.sh" | xargs -i basename {} | sed -e 's/.sh$//' | grep "_" | sort && find . -type f -name "*.sh" | xargs -i basename -s .sh {} | grep -v "_" | sort
